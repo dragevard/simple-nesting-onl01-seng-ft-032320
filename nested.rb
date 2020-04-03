c275cdd5beb298c5e570e3f5ea18da03f83bad4c
Derek phung <dragevards@gmail.com>
2020-04-03 19:00:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,8 +84,9 @@ def adding_matz
         }
      
      }
-     
-     programmer_hash[:yukihiro_matsumoto]
+      programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+      programmer_hash[:yukihiro_matsumoto][:Languages] = ["LISP", "C"]
+      programmer_hash[:yukihiro_matsumoto]
 
     
 end
